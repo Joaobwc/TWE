5d35f3e69dd5dc19285f7f18b35e4d33993fd8a4
Joaobwc <joaoa7619@gmail.com>
2025-12-17 00:15:30 +0000

    Alteração do positio para absolute e adição dos elemntos no menu brnaco para começar javascript

---
+++ tst1/template.js
@@ -0,0 +1,19 @@
+//variáveis
+const btnCarros = document.getElementById("btnCarros");
+const btnCarrinhas = document.getElementById("btnCarrinhas");
+const txtbox1 = document.getElementById("txtbox1");
+const btnDevolucao = document.getElementById("btnDevolucao");
+const dataDevolucao = document.getElementById("dataDevolucao");
+const dataRecolha = document.getElementById("dataRecolha");
+const btnMostarVeiculos = document.getElementById("btnMostarVeiculos");
+
+
+//Event listners
+//Sem funções anónimas
+btnMostarVeiculos.addEventListener("click",); 
+
+
+//Com funções anónimas
+
+
+//Funções 
