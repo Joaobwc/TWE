bbd6aadf7aa59f24c7314347e2a45344077ae6e2
JoaoCAB-beep <joaocarlosalvesbrandao@gmail.com>
2025-12-28 01:11:59 +0000

    Carrossel imagens e media querie para as 3 resoluções, 4º andar

--- tst1/template.js
+++ tst1/template.js
@@ -6,10 +6,6 @@ const btnDevolucao = document.getElementById("btnDevolucao");
 const dataDevolucao = document.getElementById("dataDevolucao");
 const dataRecolha = document.getElementById("dataRecolha");
 const btnMostarVeiculos = document.getElementById("btnMostarVeiculos");
-<<<<<<< HEAD
-=======
-
->>>>>>> 471a1eab3fcf00345be60b793235d2da2fb9361d
 const lista_aeroporto = document.getElementById("lista_aeroportos");
 const conteudo_btn = document.getElementById("conteudo_btn")
 const btnAeroporto = document.getElementById("btn_aeroporto");
@@ -25,23 +21,9 @@ const larguraMovimento = 520;
 //Sem funções anónimas
 
 //btnMostarVeiculos.addEventListener("click",);
-<<<<<<< HEAD
 
 //Com funções anónimas
 
-=======
-
-
-
-//Event listners
-//Sem funções anónimas
-btnMostarVeiculos.addEventListener("click",); 
-
-
-//Com funções anónimas
-
-
->>>>>>> 471a1eab3fcf00345be60b793235d2da2fb9361d
 //Funções 
 
 //4º Andar
@@ -244,8 +226,4 @@ if (btnMundo) {
             </div>
         `
     });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 471a1eab3fcf00345be60b793235d2da2fb9361d
